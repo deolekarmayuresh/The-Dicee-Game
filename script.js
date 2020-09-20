1c3df42597b8df9254a71618223be9dcd4e3a4c6
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-21 00:18:20 +0530

    adding random number

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // generates dice numbers from 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // generated random dice images from 1-6
+
+
