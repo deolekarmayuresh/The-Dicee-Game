0c8f23f678161804a8ee1afefd7fc256702c32c5
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-21 00:20:49 +0530

    generate image src

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; // generates dice numbers
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; // generated random dice images from 1-6
 
-
+var randomImageSource = "images/" = randomDiceImage; // generates images from source dice.png1 to dice.png6
