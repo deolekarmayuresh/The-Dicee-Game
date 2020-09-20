abe280658c26b2a74e9594bdb8e93c0fe9e396ad
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-21 00:58:26 +0530

    final changes

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 // if player 1 wins
 if (randomNumber1 > randomNumber2) {
-    document.querySelectorAll("h1").innerHTML = "🚩Congrats, Player 1 Wins!🏆"    
+    document.querySelectorAll("h1").innerHTML = "🚩Congrats, Player 1 Wins!🏆";
 }
 else if (randomNumber2 > randomNumber1) { // if player 2 wins
-    document.querySelectorAll("h1").innerHTML = "Congrats, Player 2 Wins!🚩🏆"
+    document.querySelectorAll("h1").innerHTML = "Congrats, Player 2 Wins!🚩🏆";
 }
 else { //if its a draw
-    document.querySelectorAll("h1").innerHTML = "It's a tie!🤝"
+    document.querySelectorAll("h1").innerHTML = "It's a tie!🤝";
 }
